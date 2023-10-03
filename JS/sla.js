54f9aba957064e1ae464377087d637a6e523e46f
jcindocjr144 <128598274+jcindocjr144@users.noreply.github.com>
2023-10-03 10:48:37 +0800

    Add files via upload

---
+++ JS/sla.js
@@ -0,0 +1,50 @@
+function html()
+{
+	document.getElementById('html').style.display='block';
+	document.getElementById('css').style.display="none";
+	document.getElementById('js').style.display="none";
+	document.body.style.background ="CYAN";
+}
+function css()
+{
+	document.getElementById('css').style.display="block";
+	document.getElementById('html').style.display="none";
+	document.getElementById('js').style.display="none";
+	document.body.style.background ="RED";
+}
+function js()
+{
+	document.getElementById('js').style.display="block";
+	document.getElementById('css').style.display="none";
+	document.getElementById('html').style.display="none";
+	document.body.style.background ="YELLOW";
+}
+function na()
+{
+	document.getElementById('js').style.display="none";
+	document.getElementById('css').style.display="none";
+	document.getElementById('html').style.display="none";
+}
+function home()
+{
+	document.getElementById('js').style.display="none";
+	document.getElementById('css').style.display="none";
+	document.getElementById('html').style.display="none";
+	document.body.style.background ="WHITE";
+}
+function contact()
+{
+	document.getElementById('js').style.display="none";
+	document.getElementById('css').style.display="none";
+	document.getElementById('html').style.display="none";
+	document.body.style.background ="WHITE";
+
+}
+function about()
+{
+	document.getElementById('js').style.display="none";
+	document.getElementById('css').style.display="none";
+	document.getElementById('html').style.display="none";
+	document.body.style.background ="WHITE";
+
+}
